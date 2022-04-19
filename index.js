f5930f564c933bb3b844eb94404f7eefb8c16468
brodykaren <84482063+brodykaren@users.noreply.github.com>
2022-04-19 18:07:41 -0400

    last update

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+
 // YOUR CODE HERE
+class Car{
+  constructor(make, model, year){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  honk(){
+    console.log("BEEP BEEP!")
+  }
+
+  performMaintenance(){
+    setTimeout(()=>{
+      console.log("Maintenance complete")
+    }, 3000)
+  }
+}
+
+let mySweetRide = new Car("Pontiac", "Fiero", 1988)
+
+mySweetRide.honk()
+mySweetRide.performMaintenance()
